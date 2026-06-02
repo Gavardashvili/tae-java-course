c80bb89832c208daae4fdc5cd20b73f7a7946b2a
Lizi Gavardashvili <lgavardashvili@credo.ge>
2026-06-02 22:08:33 +0400

    [Lizi Gavardashvili] HM5 - GradeCalculator

---
+++ src/main/java/Homework5/GradeCalculator.java
@@ -0,0 +1,23 @@
+/**@author Lizi Gavardashvili*/
+package Homework5;
+
+public class GradeCalculator {
+    public static String getGrade(int score){
+        if (score < 0 || score > 100) {
+            return "Invalid";
+        }
+        if (score >=90) return "A";
+        else if (score >= 80) return "B";
+        else if (score >= 70) return "C";
+        else if (score >= 60) return "D";
+        else return "F";
+
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(getGrade(75));
+
+    }
+}
