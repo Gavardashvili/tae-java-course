1e0b2ff005c30a257cead1d73f8bb98b862db087
Lizi Gavardashvili <lgavardashvili@credo.ge>
2026-06-11 00:35:05 +0400

    [Lizi Gavardashvili] HM7 - Car

---
+++ src/main/java/homework7/Car.java
@@ -0,0 +1,12 @@
+/** @author Lizi Gavardashvili*/
+package homework7;
+
+public class Car extends Vehicle{
+    public Car(String brand){
+        super(brand);
+    }
+    @Override
+    public void start() {
+        System.out.println(brand + " engine started");
+    }
+}
