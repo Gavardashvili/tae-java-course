e34469bda4f46a71dae9fac44995ecf2b936a7dd
Lizi Gavardashvili <lgavardashvili@credo.ge>
2026-06-17 01:17:26 +0400

    [Lizi Gavardashvili] HM9 - UniqueNumbers

---
+++ src/main/java/homework9/UniqueNumbers.java
@@ -0,0 +1,22 @@
+/** @author Lizi Gavardashvili*/
+package homework9;
+
+import java.util.HashSet;
+
+public class UniqueNumbers {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 4, 1, 5};
+        HashSet<Integer> numbers = new HashSet<>();
+        for(int num : arr){
+            numbers.add(num);
+        }
+
+        System.out.println("Unique: " + numbers + " Size: " + numbers.size() + " Contains 3? " +numbers.contains(3) + " Contains 10? " + numbers.contains(10));
+
+    }
+
+
+
+
+
+}
