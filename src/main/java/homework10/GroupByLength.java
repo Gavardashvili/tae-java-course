ae480407d0d16fecf59f280a3a697a650ce828db
Lizi Gavardashvili <lgavardashvili@credo.ge>
2026-06-26 00:32:34 +0400

    [Lizi Gavardashvili] HW10 - GroupByLength

---
+++ src/main/java/homework10/GroupByLength.java
@@ -0,0 +1,19 @@
+/** @author Lizi Gavardashvili*/
+package homework10;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupByLength {
+
+    public static void main(String[] args) {
+        List<String> words = List.of("a", "bb", "cc", "ddd", "ee", "fff");
+
+        Map<Integer, List<String>> result = words.stream()
+                .collect(Collectors.groupingBy(s -> s.length()));
+
+        System.out.println(result);
+
+    }
+}
