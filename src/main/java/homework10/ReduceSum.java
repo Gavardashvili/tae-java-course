bdecfd90cc9381880d3392c1e23294dbb3defd06
Lizi Gavardashvili <lgavardashvili@credo.ge>
2026-06-25 20:05:44 +0400

    [Lizi Gavardashvili] HW10 - ReduceSum

---
+++ src/main/java/homework10/ReduceSum.java
@@ -0,0 +1,15 @@
+/** @author Lizi Gavardashvili*/
+package homework10;
+
+import java.util.List;
+
+public class ReduceSum {
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1, 2, 3, 4, 5);
+        int sum = nums.stream().reduce(0, Integer::sum);
+        int product = nums.stream().reduce(1, (a, b) -> a * b);
+        int max = nums.stream().reduce(Integer::max).get();
+
+        System.out.println("Sum: " + sum + " Product: " + product+ " Max: " + max);
+    }
+}
